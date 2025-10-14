4e3d159ae0085c9d4462f64a2441c628a483be0f
Jay Miles <jomiles26@googlemail.com>
2025-10-14 14:13:58 +0100

    New file with functions to create output file

--- resources/home/dnanexus/generate_inca_vcf.py
+++ resources/home/dnanexus/generate_inca_vcf.py
@@ -1,7 +1,5 @@
 from glob import glob
-from datetime import datetime
 import os
-import config
 import subprocess
 
 if os.path.exists("/home/dnanexus"):
@@ -10,15 +8,19 @@ if os.path.exists("/home/dnanexus"):
         ["pip", "install", "--no-index", "--no-deps"] + glob("packages/*")
     )
 
-import pandas as pd
 import argparse
 import dxpy
-import pysam
-import pysam.bcftools
 from utils.parse_input import (
     clean_csv,
     filter_probeset,
     aggregate_uniq_vars
+)
+from utils.generate_output import (
+    create_output_filename,
+    initialise_vcf,
+    write_vcf_header,
+    index_file,
+    bcftools_annotate_vcf
 )
 
 
@@ -80,109 +82,6 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-def intialise_vcf(aggregated_df, minimal_vcf) -> None:
-    """
-    Initialise minimal VCF with CHROM, POS, ID, REF, ALT with minimal header
-
-    Parameters
-    ----------
-    aggregated_df : pd.DataFrame
-        Dataframe of aggregated data
-    minimal_vcf : str
-        Output filename for the minimal VCF
-    """
-    vcf_lines = []
-    for _, row in aggregated_df.iterrows():
-        vcf_line = (
-            f"{row['CHROM']}\t{row['POS']}\t.\t{row['REF']}\t{row['ALT']}\t.\t.\t."
-        )
-        vcf_lines.append(vcf_line)
-
-    with open(minimal_vcf, "w") as vcf_file:
-        vcf_file.write(config.MINIMAL_VCF_HEADER)
-        vcf_file.write("\n".join(vcf_lines) + "\n")
-
-
-def write_vcf_header(db, genome_build, header_filename) -> None:
-    """
-    Write VCF header by populating INFO fields and specifying contigs
-
-    Parameters
-    ----------
-    genome_build : str
-        Genome build to specify contigs in header
-    header_filename : str
-        Output filename for the VCF header
-    """
-    if db == 'inca':
-        config_field = config.INFO_FIELDS_INCA
-    else:
-        config_field = config.INFO_FIELDS_VARSTORE
-
-    with open(header_filename, "w") as header_vcf:
-        for field_info in config_field.values():
-            info_line = f'##INFO=<ID={field_info["id"]},Number={field_info["number"]},Type={field_info["type"]},Description="{field_info["description"]}">\n'
-            header_vcf.write(info_line)
-
-        if genome_build == "GRCh37":
-            header_vcf.write(config.GRCh37_CONTIG)
-        else:
-            header_vcf.write(config.GRCh38_CONTIG)
-
-
-def index_file(file) -> None:
-    """
-    Bgzip and index file
-
-    Parameters
-    ----------
-    file : str
-        File to bgzip and index
-    """
-    pysam.tabix_compress(f"{file}", f"{file}.gz")
-    pysam.tabix_index(f"{file}.gz", seq_col=0, start_col=1, end_col=1)
-
-
-def bcftools_annotate_vcf(
-    db, aggregated_database, minimal_vcf, header_filename, output_filename
-) -> None:
-    """
-    Run bcftools annotate to annotate the minimal VCF with the aggregated info
-
-    Parameters
-    ----------
-    aggregated_database : str
-        Output filename of aggregated database
-    minimal_vcf : str
-        Output filename for the minimal VCF
-    header_filename : str
-        Output filename for the VCF header
-    output_filename : str
-        Output filename for annotated VCF
-    """
-    if db == 'inca':
-        config_field = config.INFO_FIELDS_INCA
-    else:
-        config_field = config.INFO_FIELDS_VARSTORE
-
-    info_fields = ",".join(item["id"] for item in config_field.values())
-
-    # Run bcftools annotate with pysam
-    annotate_output = pysam.bcftools.annotate(
-        "-a",
-        f"{aggregated_database}.gz",
-        "-h",
-        f"{header_filename}",
-        "-c",
-        f"CHROM,POS,REF,ALT,{info_fields}",
-        f"{minimal_vcf}",
-    )
-    with open(output_filename, "w") as f:
-        f.write(annotate_output)
-
-    index_file(output_filename)
-
-
 def download_input_file(remote_file) -> str:
     """
     Download given input file with same name as file in project
@@ -238,35 +137,6 @@ def upload_output_file(outfile) -> None:
     return dxpy.dxlink(url_file)
 
 
-def create_output_filename(database, genome_build, probeset):
-    """
-    Generate an output filename if none is provided
-
-    Parameters
-    ----------
-    database : str
-        inca or variant_store
-    genome_build : str
-        GRCh37 or GRCh38
-    probeset : str
-        germline or somatic
-
-    Returns
-    -------
-    str
-        Name for output VCF
-    """
-    date = datetime.today().strftime("%y%m%d")
-    output = f"{date}_{database}_{genome_build}"
-
-    if probeset:
-        output += f"_{probeset}"
-
-    output += ".vcf"
-
-    return output
-
-
 @dxpy.entry_point("main")
 def main(database: str, input_file: str, output_filename: str, genome_build: str, probeset: str):
 
@@ -290,7 +160,7 @@ def main(database: str, input_file: str, output_filename: str, genome_build: str
         filtered_df = initial_df
     aggregated_df = aggregate_uniq_vars(database, filtered_df, aggregated_database)
 
-    intialise_vcf(aggregated_df, minimal_vcf)
+    initialise_vcf(aggregated_df, minimal_vcf)
     write_vcf_header(database, genome_build, header_filename)
     index_file(aggregated_database)
     bcftools_annotate_vcf(
